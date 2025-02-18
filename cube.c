758bd3eabb77b2fc13d35eb4751cc299e8fac13a
chris <chris.ehmann5@gmail.com>
2025-02-18 00:00:19 -0800

    Create interactive display

--- cube.c
+++ cube.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
+#define RESET "\x1b[0m"
 int NUM_MOVES = 18;
 
 const char *faces[] = { "U", "D", "F", "B", "L", "R",
                         "U\'", "D\'", "F\'", "B\'", "L\'", "R\'",
                         "U2", "D2", "F2", "B2", "L2", "R2" };
+
+const char *colours[] = {"W", "Y", "R", "O", "G", "B"};
+
+const char *text_colours[] = {"\033[0;37m", "\033[0;33m", "\x1b[31m", "\033[0;36m", "\033[0;32m", "\033[0;34m"};
 
 /* NOTATION
 ** Right moves never follow Left (RL allowed, LR not)
@@ -14,7 +20,7 @@ const char *faces[] = { "U", "D", "F", "B", "L", "R",
 ** Front never follows back (FB allowed, BF not)
 ** After an UP turn, we are allowed: D L R F B D' L' R' F' B' D2 L2 R2 F2 B2
 ** AFTER a DOWN turn, we are allowed: F B L R F' B' L' R' L2 R2 F2 B2
-** After a FRONT turn, we are allowed: U D L R B
+** etc, etc...
 */
 int transition_table[7][18] = {
     {1,2,3,4,5,7,8,9,10,11,13,14,15,16,17,0,0,0},
@@ -46,25 +52,52 @@ int create_scramble(int * cube, int * scramble) {
 
     for(int i = 0; i < scramble_length; i++) {
         int direction = (int)((double)rand() / ((double)RAND_MAX + 1.0) * ((double)NUM_MOVES - 1.0));
-        turn(direction, cube);
         scramble[i] = direction;
     }
 
     return scramble_length;
 }
 
+
+
 void print_cube(int * cube) {
-    for(int face = 0; face < 6; face++) {
-        printf("%s: \n", faces[face]);
-        for(int row = 0; row < 3; row++) {
-            for(int col = 0; col < 3; col++) {
-                printf("%d ", cube[face * 9 + row * 3 + col]);
-            }
-            printf("\n");
+    for(int i = 0; i < 3; i++) {
+        printf("      ");
+        for(int j = 0; j < 3; j++) {
+            int curr_facelet = cube[i * 3 + j];
+            printf(text_colours[curr_facelet]);
+            printf("%s ", colours[curr_facelet]);
+            printf(RESET);
         }
         printf("\n");
     }
 
+    int faces[] = {4,2,5,3};
+
+    for(int row = 0; row < 3; row++) {
+        for(int face = 0; face < 4; face++) {
+            for(int cubelet = 0; cubelet < 3; cubelet++) {
+                int curr_facelet = cube[faces[face] * 9 + row * 3 + cubelet];
+                printf(text_colours[curr_facelet]);
+                printf("%s ", colours[curr_facelet]);
+                printf(RESET);
+            }
+        }
+        printf("\n");
+    }
+
+    for(int i = 0; i < 3; i++) {
+        printf("      ");
+        for(int j = 0; j < 3; j++) {
+            int curr_facelet = cube[9 + i * 3 + j];
+            printf(text_colours[curr_facelet]);
+            printf("%s ", colours[curr_facelet]);
+            printf(RESET);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
     return;
 }
 
--- main.c
+++ main.c
@@ -5,14 +5,28 @@
 #include "cube_perms.h"
 #include "cube.h"
 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+
 int main()
 {
+    system("cls");
+    srand ( time(NULL) );
+    
     int cube[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0,
                    1, 1, 1, 1, 1, 1, 1, 1, 1,
                    2, 2, 2, 2, 2, 2, 2, 2, 2,
                    3, 3, 3, 3, 3, 3, 3, 3, 3, 
                    4, 4, 4, 4, 4, 4, 4, 4, 4,
                    5, 5, 5, 5, 5, 5, 5, 5, 5 };
+
+    const char *faces[] = { "U", "D", "F", "B", "L", "R",
+                        "U\'", "D\'", "F\'", "B\'", "L\'", "R\'",
+                        "U2", "D2", "F2", "B2", "L2", "R2" };
 
     int scramble[64];
     int solve[64];
@@ -21,33 +35,46 @@ int main()
     clear_scramble(scramble);
 
     int curr_scramble_length = create_scramble(cube, scramble);
-
     print_cube(cube);
-    print_scramble(curr_scramble_length, scramble);
-    int max_depth = 12;
+
+    for(int i = 0; i < curr_scramble_length; i++) {
+        turn(scramble[i], cube);
+        system("cls");
+        print_cube(cube);
+        Sleep(150);
+    }
+
+    printf("Scrambled!\n\n");
+
+    char t[2];
 
     while(1) {
-        for(int i = 0; i <= max_depth; i++) {
-            clock_t start, stop;
-            printf("Current depth: %d\n", i);
+        printf("Enter turn: ");
+        scanf("%s", &t);
+        printf("\n");
+        int next_turn = -1;
 
-            start = clock();
-            int result = solve_cube(cube, i);
-            stop = clock();
+        while(next_turn == -1) {
+            for(int i = 0; i < 18; i++) {
+                if(strcmp(faces[i], t) == 0) {
+                    next_turn = i;
+                }
+            }
 
-            float duration = ( float ) ( stop - start ) / CLOCKS_PER_SEC;
-
-            printf("Duration for depth %d: %f seconds\n", i, duration);
-
-            if(result) {
-                printf("Cube solved with a depth of %d\n", i);
-                break;
+            if(next_turn == -1) {
+                printf("Not a valid turn, please enter a valid turn: ");
+                scanf("%s", &t);
+                printf("\n");
             }
         }
-        printf("No solution found, trying again...\n");
-        reset_cube(cube);
-        clear_scramble(scramble);
-        curr_scramble_length = create_scramble(cube, scramble);
+
+        turn(next_turn, cube);
+        system("cls");
+        print_cube(cube);
+
+        if(is_solved(cube)) {
+            printf("Solved!");
+        }
     }
 
     return 0;
